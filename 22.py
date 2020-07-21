71b1a7bdadf5fe047ab23f468309b04caba98bca
quasar2yh <beta2020@naver.com>
2020-07-21 11:02:04 +0900

    0721 code

---
+++ 22.py
@@ -0,0 +1,29 @@
+import sys
+
+def comp(seq: str)->str:
+    comp = ''
+    for s in seq:
+        if s=='A':
+            comp += 'T'
+        elif s == 'T':
+            comp += 'A'
+        elif s == 'G':
+            comp += 'C'
+        elif s == 'C':
+            comp +='G'
+    return comp
+
+def comp2(seq: str)->str:
+    d_comp = {'A':'T','G':'C','C':'G','T':'A'}
+    comp=''
+    for s in seq:
+        comp += d_comp[s]
+    return comp
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f'#usage: python{sys.argv[0]} [string]')
+        sys.exit()
+    seq = sys.argv[1]
+    res = comp(seq)
+    print(res)
